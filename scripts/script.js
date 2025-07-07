3e10f2a4b5cb378cb35286571a11455cd0cebd30
Davis <helmsdavis@gmail.com>
2025-07-07 18:48:25 -0400

    prep-reformatting

--- scripts/script.js
+++ scripts/script.js
@@ -2,13 +2,13 @@
 function toggleHeader() {
 	//scroll to top
 	window.scrollTo(0, 0);
-	
+
 	var e = document.getElementById('headerContent');
 	var f = document.getElementById('headerFail');
 	var g = document.getElementById('mobileMenu');
 
 	//toggle header
-	if(e.style.display == 'block') {
+	if (e.style.display == 'block') {
 		e.style.display = 'none';
 		f.style.display = 'none';
 		g.src = 'images/menu.png';
@@ -24,13 +24,13 @@ function toggleHeader() {
 function toggleHeaderArtWork() {
 	//scroll to top
 	window.scrollTo(0, 0);
-	
+
 	var e = document.getElementById('headerContent');
 	var f = document.getElementById('headerFail');
 	var g = document.getElementById('mobileMenu');
 
 	//toggle header
-	if(e.style.display == 'block') {
+	if (e.style.display == 'block') {
 		e.style.display = 'none';
 		f.style.display = 'none';
 		g.src = '../images/menu.png';
@@ -44,21 +44,21 @@ function toggleHeaderArtWork() {
 
 //click to copy text
 function copyToClipboard(id, notification) {
-	
+
 	//set up area that can be copied
 	var copyText = document.createRange();
 	copyText.selectNode(document.getElementById(id));
-	
+
 	//copy the text
 	window.getSelection().removeAllRanges();
 	window.getSelection().addRange(copyText);
 	document.execCommand('copy');
 	window.getSelection().removeAllRanges();
-	
+
 	//user feedback confirming that the text was copied
 	var popup = document.getElementsByClassName(notification)[0];
 	popup.style.opacity = "1";
-	setTimeout(function() {
+	setTimeout(function () {
 		popup.style.opacity = "0";
 	}, 3000)
 }
